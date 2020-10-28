9374e1a2752e3af4bd29a5ca0c594874744ce9ae
Skyler Tosh <skylerjtosh@gmail.com>
2020-10-28 14:35:11 -0700

    Added base functionality for login button

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,18 @@
 import React, {useRef} from 'react';
 import { Container, Form, Button } from 'react-bootstrap'
 
-export default function Login() {
+export default function Login({ onIdSubmit }) {
     const idRef = useRef()
+
+    function handleSubmit(e) {
+        e.preventDefault()
+
+        onIdSubmit(idRef.current.value)
+    }
 
     return (
         <Container className="align-items-center d-flex" style={{ height: '100vh'}}>
-            <Form className="w-100">
+            <Form onSubmit={handleSubmit} className="w-100">
                 <Form.Group>
                     <Form.Label>Enter Your ID</Form.Label>
                     <Form.Control type="text" ref={idRef} required />
