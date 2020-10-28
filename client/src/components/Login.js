fc1ea67d6f2fde510038a143926d6a11146b697b
Skyler Tosh <skylerjtosh@gmail.com>
2020-10-28 14:40:03 -0700

    Added uuid as a dependency

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,6 @@
 import React, {useRef} from 'react';
 import { Container, Form, Button } from 'react-bootstrap'
+import { v4 from uuidV4 } from 'uuid'
 
 export default function Login({ onIdSubmit }) {
     const idRef = useRef()
