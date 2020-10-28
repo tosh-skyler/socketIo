e2369a7294d0c04ec1f987eba2291b5059c5ccd8
Skyler Tosh <skylerjtosh@gmail.com>
2020-10-28 14:23:41 -0700

    Created login form.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import { Container, Form, Button } from 'react-bootstrap'
 
 export default function Login() {
+    const idRef = useRef()
+
     return (
-        <div>
-            
-        </div>
+        <Container className="align-items-center d-flex" style={{ height: '100vh'}}>
+            <Form className="w-100">
+                <Form.Group>
+                    <Form.Label>Enter Your ID</Form.Label>
+                    <Form.Control type="text" ref={idRef} required />
+                </Form.Group>
+                <Button type="submit" className="mr-2">Login</Button>
+                <Button variant="secondary">Create a New ID</Button>
+            </Form>
+        </Container>
     )
 }
