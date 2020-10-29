21a94753d8a737b87b963871c91afb9d0530d0f9
Skyler Tosh <skylerjtosh@gmail.com>
2020-10-28 17:32:08 -0700

    createNewId now generates a randomized user id

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,6 @@
 import React, {useRef} from 'react';
 import { Container, Form, Button } from 'react-bootstrap'
-import { v4 from uuidV4 } from 'uuid'
+import { v4 as uuidV4 } from 'uuid'
 
 export default function Login({ onIdSubmit }) {
     const idRef = useRef()
@@ -11,6 +11,10 @@ export default function Login({ onIdSubmit }) {
         onIdSubmit(idRef.current.value)
     }
 
+    function createNewId() {
+        onIdSubmit(uuidV4())
+    }
+
     return (
         <Container className="align-items-center d-flex" style={{ height: '100vh'}}>
             <Form onSubmit={handleSubmit} className="w-100">
@@ -19,7 +23,7 @@ export default function Login({ onIdSubmit }) {
                     <Form.Control type="text" ref={idRef} required />
                 </Form.Group>
                 <Button type="submit" className="mr-2">Login</Button>
-                <Button variant="secondary">Create a New ID</Button>
+                <Button onClick={createNewId} variant="secondary">Create a New ID</Button>
             </Form>
         </Container>
     )
