301418707c575e3902d422af369339301d9eb833
Skyler Tosh <skylerjtosh@gmail.com>
2020-10-28 19:03:26 -0700

    Added conversation module.

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Tab, Nav } from 'react-bootstrap'
+import Conversations from './Conversations.js'
+
+const CONVERSATOINS_KEY = 'conversations';
+const CONTACTS_KEY = 'contacts';
 
 export default function Sidebar({ id }) {
+    const [activeKey, setActiveKey] = useState(CONVERSATOINS_KEY)
+
     return (
-        <div>
-            Hello there!
+        <div style={{ width: '250px' }} className="d-flex flex-column">
+            <Tab.Container activeKey={activeKey} onSelect={setActiveKey}>
+                <Nav variant="tabs" className="justify-content-center">
+                    <Nav.Item>
+                        <Nav.Link eventKey={CONVERSATOINS_KEY}>Conversations</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey={CONTACTS_KEY}>Contacts</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+                <Tab.Content>
+                    <Tab.Pane eventKey={CONVERSATOINS_KEY}>
+                        <Conversations />
+                    </Tab.Pane>
+                </Tab.Content>
+            </Tab.Container>
         </div>
     )
 }
