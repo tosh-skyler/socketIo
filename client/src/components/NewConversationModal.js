c26487e2518b7e843e50c7663fee6884a6ac7278
Skyler Tosh <skylerjtosh@gmail.com>
2020-10-29 19:38:48 -0700

    Create contact now works!

--- client/src/components/NewConversationModal.js
+++ client/src/components/NewConversationModal.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Modal, Form, Button } from 'react-bootstrap'
 
 export default function NewConversationModal() {
     return (
         <div>
-            
+            Conversations
         </div>
     )
 }
