b372adde2c6af593e2e4f8ad0f1f417974a2b2ca
Skyler Tosh <skylerjtosh@gmail.com>
2020-11-01 15:12:46 -0800

    Create conversation modal now opens, but you can't create conversations yet.

---
+++ client/src/contexts/ConversationsProvider.js
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react'
+import useLocalStorage from '../hooks/useLocalStorage'
+
+const ConversationsContext = React.createContext()
+
+export function useConversations() {
+    return useContext(ConversationsContext)
+}
+
+export function ConversationsProvider({ children }) {
+    const [conversations, setConversations] = useLocalStorage('conversations', [])
+
+    function createConversation(recipients) {
+        setConversations(prevConversations => {
+          return [...prevConversations, { recipients, messages: [] }]
+        })
+    }
+
+    return (
+        <ConversationsContext.Provider value={{ conversations, createConversation}}>
+            {children}
+        </ConversationsContext.Provider>
+    )
+}
